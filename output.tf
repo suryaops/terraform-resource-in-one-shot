bbf48cf22531d9d5b478920b4e0ddb326f7c50ec
suraj <135117044+suryaops@users.noreply.github.com>
2025-03-30 00:30:38 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,15 +1,3 @@
-output "cluster_id" {
-  value = aws_eks_cluster.cloudpunch.id
-}
-
-output "node_group_id" {
-  value = aws_eks_node_group.cloudpunch.id
-}
-
-output "vpc_id" {
-  value = aws_vpc.cloudpunch_vpc.id
-}
-
 resource "aws_s3_bucket_object" "object" {
   bucket = "mybuckets210899"
   key    = "new_object_key"
