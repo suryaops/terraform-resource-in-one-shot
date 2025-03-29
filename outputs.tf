ddaef7445369b9c373a0a6a7bac2059b48713326
suraj <135117044+suryaops@users.noreply.github.com>
2025-03-30 00:29:22 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,1 @@
-output "cluster_id" {
-  value = aws_eks_cluster.cloudpunch.id
-}
-
-output "node_group_id" {
-  value = aws_eks_node_group.cloudpunch.id
-}
-
-output "vpc_id" {
-  value = aws_vpc.cloudpunch_vpc.id
-}
 
