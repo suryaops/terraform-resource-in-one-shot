4c3b43ea3e992a3b320a764da59e6ad0408068a6
suraj <135117044+suryaops@users.noreply.github.com>
2025-03-30 00:24:58 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,22 @@
+output "cluster_id" {
+  value = aws_eks_cluster.cloudpunch.id
+}
+
+output "node_group_id" {
+  value = aws_eks_node_group.cloudpunch.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.cloudpunch_vpc.id
+}
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = "mybuckets210899"
+  key    = "new_object_key"
+  source = "/root/terraform/s3object"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("/root/terraform/s3object")
+}
